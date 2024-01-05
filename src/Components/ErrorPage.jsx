c673f2c82b77cd87030136ee830f7615d5228608
Antonio <antonio.ibarra99@outlook.es>
2024-01-05 12:21:40 -0800

    Font fix v5.6 Final

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -8,8 +8,9 @@ export default function ErrorPage() {
       <h1 className="text-center text-5xl font-extrabold mt-20 text-blue-900">
         CRM - CLIENTES ✨
       </h1>
-      <p className="text-center">Ocurrio el siguiente error: </p>
+      <p className="text-center">Ocurrió el siguiente error: </p>
       <p className="text-center">{error.statusText || error.message}</p>
+      <p className="text-center">No hay datos para mostrar</p>
     </div>
   );
 }
